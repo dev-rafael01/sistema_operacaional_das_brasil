a4ff07953a2c6ba821d1a9fb50b937e4cc9259f0
Rafael Santos <rafaelsantos.js30@gmail.com>
2026-03-04 21:59:04 -0300

    colocando responsividade no codigo

--- app/views/layouts/header.php
+++ app/views/layouts/header.php
@@ -11,6 +11,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
      <!-- <link rel="stylesheet" href="/sistemaoperacionaldasbrasil/public/assets/style.css"> -->
       <link rel="stylesheet" href="/sistemaoperacionaldasbrasil/public/assets/style.css?v=<?= time() ?>">
+      <link rel="stylesheet" href="/sistemaoperacionaldasbrasil/public/assets/responsive.css<?time() ?>">
      
     <title>Sistem Operacional Das Brasil</title>
  
